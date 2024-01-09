48e70e51cead9563b983d66add1ac87022932279
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-01-09 18:55:07 +0200

    Add CI matrix

---
+++ samples/without_ota/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+export BOARD=$1
+
+case $BOARD in
+    adafruit_feather_m0_basic_proto|arduino_due)
+        west build -p -b ${BOARD} \
+            -DEXTRA_CONF_FILE=$(pwd)/boards/adafruit_airlift.conf
+        ;;
+    *) # Invalid option
+        echo "Error: Invalid option"
+        exit 1
+esac
