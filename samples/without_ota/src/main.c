0d9d2ef524715d7bfc501b5d65a3c6738cfe0a3f
andrii yarmolenko <andrii.yarmolenko@blynk.cc>
2024-01-11 13:43:53 +0200

    send uptime each second

--- samples/without_ota/src/main.c
+++ samples/without_ota/src/main.c
@@ -81,7 +81,7 @@ int main(void)
     rpc_blynk_setConfigTimeout(30*60);
 
     // Start a periodic timer
-    k_timer_start(&periodic_timer, K_SECONDS(10), K_SECONDS(10));
+    k_timer_start(&periodic_timer, K_SECONDS(1), K_SECONDS(1));
 
     k_sleep(K_FOREVER);
 
